563a79e6e1a1fcd26210de98a1c94fa1d5d8a1bc
nicolas <nmr@adhoc.com.ar>
2022-01-06 12:21:22 -0300

    [FIX] runbot_ux: add chatters and activities

---
+++ runbot_ux/models/runbot_trigger.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+
+class RunbotTrigger(models.Model):
+    _name = 'runbot.trigger'
+    _inherit = ['runbot.trigger', 'mail.activity.mixin']
+
+    project_id = fields.Many2one(tracking=True)
+    config_id = fields.Many2one(tracking=True)
+    repo_ids = fields.Many2many(tracking=True) 
+    dependency_ids = fields.Many2many(tracking=True) 
+    version_domain = fields.Char(tracking=True)
