9729a27134ef9ec9624e2fa2316df564e342472a
filoquin <filoquin@gmail.com>
2022-12-06 08:22:01 -0300

    [MIG] runbot_merge_ux: Migration to 15.0

--- runbot_merge_ux/__manifest__.py
+++ runbot_merge_ux/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "runbot_merge_ux",
     'summary': "Runbot",
@@ -6,7 +5,7 @@
     'author': "ADHOC SA",
     'website': "http://runbot.odoo.com",
     'category': 'Website',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'depends': ['runbot', 'runbot_merge'],
     'data': [
         'views/templates.xml',
