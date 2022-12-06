27dabf31eb3a20042181de038e740afde03443cc
filoquin <filoquin@gmail.com>
2022-12-06 08:21:40 -0300

    [MIG] runbot_transifex: Migration to 15.0

--- runbot_transifex/__manifest__.py
+++ runbot_transifex/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Tunbot Transifex",
     'summary': "Runbot Transifex",
@@ -6,7 +5,7 @@
     'author': "ADHOC SA",
     'website': "http://runbot.odoo.com",
     'category': 'Website',
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'depends': ['runbot'],
     'data': [
         'views/transifex_project_views.xml',
--- runbot_transifex/models/__init__.py
+++ runbot_transifex/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import runbot_branch
 from . import transifex_project
