effed144fc33c0caba8a729bfe8ed3ebf53bf087
filoquin <filoquin@gmail.com>
2022-12-07 11:42:46 -0300

    [WIP] Agrego id al tree del from runbot.repo_form [DEL] Elimnino action server que agrega json al context

--- runbot_ux/models/__init__.py
+++ runbot_ux/models/__init__.py
@@ -2,7 +2,6 @@
 from . import runbot_project
 from . import runbot_version
 from . import runbot_build
-from . import ir_actions_server
 from . import runbot_repo
 from . import runbot_trigger
 from . import runbot_build_params
