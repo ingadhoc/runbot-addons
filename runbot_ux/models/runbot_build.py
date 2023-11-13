5076a652eff3cea2f1f80f28e79a7fdeca0358b8
Juan José Scarafia <jjs@adhoc.com.ar>
2023-11-13 17:14:23 -0300

    [IMP] runbot_ux: patch _get_addons_path

--- runbot_ux/models/runbot_build.py
+++ runbot_ux/models/runbot_build.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+import os
+from odoo import models
+from odoo.tools import config
 from odoo.tools.safe_eval import safe_eval
 
 
@@ -12,3 +14,14 @@ class RunbotBuild(models.Model):
         if self.version_id.custom_pre_commands:
             command.pres += safe_eval(self.version_id.custom_pre_commands)
         return command
+
+    def _get_addons_path(self):
+        """ parche de metodo nativo para que si el path no es valido no lo pase como addons path porque odoo no levanta
+        esto es practico para nuevas versiones donde todavia no hay modulos carpetas dentro (caso oca que se inicializan vacios)"""
+        for commit in (self.env.context.get('defined_commit_ids') or self.params_id.commit_ids):
+            if not commit.repo_id.manifest_files:
+                continue  # skip repo without addons
+            source_path = self._docker_source_folder(commit)
+            for addons_path in (commit.repo_id.addons_paths or '').split(','):
+                if os.path.isdir(commit._source_path(addons_path)) and config._is_addons_path(commit._source_path(addons_path)):
+                    yield os.path.join(source_path, addons_path).strip(os.sep)
