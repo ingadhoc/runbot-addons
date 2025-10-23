361a5f0c01b4f637ec373fc1b17f7eee55a8dc09
Virginia <vib@adhoc.inc>
2025-10-23 20:14:48 +0000

    [FIX] runbot_trnasifex: decode translated content as UTF-8 in sync process

--- runbot_transifex/models/runbot_branch.py
+++ runbot_transifex/models/runbot_branch.py
@@ -130,7 +130,7 @@ class RunbotBranch(models.Model):
                     url = transifex_api.ResourceTranslationsAsyncDownload.download(
                         resource=tx_resource, language=tx_language
                     )
-                    translated_content = requests.get(url, timeout=30).text
+                    translated_content = requests.get(url, timeout=30).content.decode("utf-8")
 
                     if translated_content:
                         gh_i18n_path = os.path.join("/", tx_resource.slug, "i18n")
