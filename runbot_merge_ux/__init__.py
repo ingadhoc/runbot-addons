418fdaa70d067f9f0b149e8491f731b0a97de35a
Virginia <vib@adhoc.inc>
2026-01-22 15:09:59 +0000

    [ADD] Implement Runbot Merge UX module with version bump policy and feedback system

---
+++ runbot_merge_ux/__init__.py
@@ -0,0 +1,7 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+
+from . import models
+from .patch import post_load
