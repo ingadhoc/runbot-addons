25124f8e79e4b7cb8c6e69a1a73ef569da52445f
roboadhoc <roboadhoc@adhoc.com.ar>
2026-05-05 12:04:18 +0000

    [BOT] Bump version: runbot_ux 18.0.1.1.0
    Merged: ingadhoc/runbot-addons#47

--- runbot_ux/__manifest__.py
+++ runbot_ux/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "ADHOC SA",
     "website": "http://runbot.odoo.com",
     "category": "Website",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "depends": ["runbot"],
     "data": [
         "views/config_step_views.xml",
