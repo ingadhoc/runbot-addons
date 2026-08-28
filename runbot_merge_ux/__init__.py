65e7403d70fe2264e6650b8a5e6c1da673c2c160
Virginia <vib@adhoc.inc>
2026-08-28 13:18:42 +0000

    [REM] runbot_merge_ux: remove from 18.0
    The mergebot runs on the 17.0 branch, where this module is actively
    maintained (17.0.0.3.0). The copy on this branch was left at the initial
    development state (17.0.0.0.0) and never received the later fixes.
    
    closes ingadhoc/runbot-addons#65
    
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

--- runbot_merge_ux/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import controllers
-from . import models
-from .patch import post_load
